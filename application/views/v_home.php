ed5365e91d30fd82ee7f90532454cc55a1e36225
joseminaya15 <jose.minayac15@gmail.com>
2018-11-26 11:48:03 -0500

    public

---
+++ application/views/v_home.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible"  content="IE=edge">
+    <meta name="viewport"               content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <meta name="description"            content="HPE Simplivity - Intel">
+    <meta name="keywords"               content="HPE Simplivity - Intel">
+    <meta name="robots"                 content="Index,Follow">
+    <meta name="date"                   content="August 01, 2018"/>
+    <meta name="language"               content="es">
+    <meta name="theme-color"            content="#000000">
+	<title>HPE Intel NSX - SDDC</title>
+    <link rel="shortcut icon" href="<?php echo RUTA_IMG?>logo/favicon.ico">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>toaster/toastr.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>mdl/material.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap-select/css/bootstrap-select.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_PLUGINS?>bootstrap/css/bootstrap.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>material-icons.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_FONTS?>metric.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>m-p.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet"    href="<?php echo RUTA_CSS?>style.css?v=<?php echo time();?>">
+</head>
+<body>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="Home">Home</a></li>
+                <li><a href="Story">The Story</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="Design">The design</a></li>
+                <li><a href="Collection">Collection</a></li>
+                <li><a href="#">Share with us</a></li>
+                <li><a href="#">Contact us</a></li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    <!-- <div class="js-header">
+        <div class="js-header--container">
+            <div class="js-header--left">
+                <img src="<?php echo RUTA_IMG?>logo/hpe-intel.png">
+            </div>
+        </div>
+    </div> -->
+    <section id="home">
+        <div class="js-fondo"></div>
+        <div class="js-container">
+            <div class="js-home js-flex">
+                <div class="js-contenido">
+                    <h2>Gane agilidad con un datacenter definido por software - SDDC</h2>
+                    <div class="js-xeon">
+                        <img src="<?php echo RUTA_IMG?>logo/xeon-logo.png">
+                        <p>Familia de procesadores escalables Intel&reg; Xeon&reg;</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="register" class="js-section">
+        <div class="js-container">
+            <div class="js-information">
+                <h2>Jueves 4 de Octubre, 2018</h2>
+                <h3>4:30 pm a 6:30 pm</h3>
+                <p>Tribeca, Costa Rica</p>
+                <span>Del Ceviche del Rey 2000 norte y 200 este. Casa esquinera a mano izquierda. Calle Lajas, Santa Ana, San Jose</span>
+            </div>
+            <div class="js-title">
+                <h2>Reg&iacute;strese completando el siguiente formulario</h2>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="js-input">
+                        <label for="text">Nombre*</label>
+                        <input type="text" id="name">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class="js-input">
+                        <label for="text">Apellido*</label>
+                        <input type="text" id="surname">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class=" js-input">
+                        <label for="text">Email*</label>
+                        <input type="text" id="email" >
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class=" js-input">
+                        <label for="text">Teléfono*</label>
+                        <input type="text" id="phone">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                     <div class=" js-input">
+                        <label for="text">Empresa*</label>
+                        <input type="text" id="company" >
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class=" js-input">
+                        <label for="text">Cargo*</label>
+                        <input type="text" id="position">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4">
+                    <div class=" js-input">
+                        <label for="text">País*</label>
+                        <input type="text" id="country">
+                    </div>
+                </div>
+            </div>
+            <div class="js-terminos">
+                <p>Le gustar&iacute;a recibir comunicaciones personalizadas sobre HPE y sobre productos, servicios, ofertas y eventos seleccionados de partners de HPE?</p>
+                <div class="js-radio">
+                    <label class="js-label__name">Correo electr&oacute;nico*</label>
+                    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="electronico">
+                        <input type="radio" id="electronico" class="mdl-radio__button" name="options" value="1">
+                        <span class="mdl-radio__label">Si</span>
+                    </label>
+                    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="noelectronico">
+                        <input type="radio" id="noelectronico" class="mdl-radio__button" name="options" value="2">
+                        <span class="mdl-radio__label">No</span>
+                    </label>
+                </div>
+                <div class="js-radio">
+                    <label class="js-label__name">Tel&eacute;fono*</label>
+                    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="telefono">
+                        <input type="radio" id="telefono" class="mdl-radio__button" name="options1" value="1">
+                        <span class="mdl-radio__label">Si</span>
+                    </label>
+                    <label class="mdl-radio mdl-js-radio mdl-js-ripple-effect" for="notelefono">
+                        <input type="radio" id="notelefono" class="mdl-radio__button" name="options1" value="2">
+                        <span class="mdl-radio__label">No</span>
+                    </label>
+                </div>
+                <p>Para m&aacute;s informaci&oacute;n sobre c&oacute;mo HPE gestiona, usa y protege su informaci&oacute;n, consulte la <a href="https://www.hpe.com/lamerica/es/legal/privacy.html" target="_blank" id="">Declaraci&oacute;n de privacidad de HPE.</a> Siempre puede anular o modificar su consentimiento para recibir comunicaciones de marketing de HPE. Podr&aacute; hacerlo mediante las opciones de exclusi&oacute;n voluntaria y preferencias que se encuentran en la parte inferior de nuestras comunicaciones de marketing por correo electr&oacute;nico o bien, siguiendo el <a href="https://h41360.www4.hpe.com/unsubscribe-gp.php" target="_blank">enlace a continuaci&oacute;n.</a>Si proporcion&oacute; su n&uacute;mero de tel&eacute;fono m&oacute;vil para recibir comunicaciones de marketing, tenga en cuenta que podr&iacute;n aplicarse cargos por roaming.</p>
+            </div>
+            <div class="js-section--button text-left">
+                <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect js-button js-login" onclick="sendInformation()">Enviar</button>
+            </div>
+            <div id="confirmation" class="js-confirmation">
+                <h2>Registro realizado correctamente. Lo esperamos</h2>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="js-container text-center">
+            <p class="content">Intel y el logotipo de Intel son marcas comerciales de la Corporación Intel o sus filiales en los Estados Unidos o en otros países</p>
+            <p>&copy;2018 Copyright Hewlett Packard Enterprise Development LP</p>
+        </div>
+    </footer>
+	<script src="<?php echo RUTA_JS?>jquery-3.2.1.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jquery-1.11.2.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/bootstrap-select.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>bootstrap-select/js/i18n/defaults-es_ES.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>mdl/material.min.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_PLUGINS?>toaster/toastr.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>Utils.js?v=<?php echo time();?>"></script>
+    <script src="<?php echo RUTA_JS?>jsindex.js?v=<?php echo time();?>"></script>
+    <script type="text/javascript">
+        // var URLactual = window.location;
+        // if(URLactual['href'] != 'http://www.marketinghpe.com/microsite/nsx_sddc/cr/'){
+        //     location.href = 'http://www.marketinghpe.com/microsite/nsx_sddc/cr/';
+        // }
+    </script>
+</body>
+</html>
